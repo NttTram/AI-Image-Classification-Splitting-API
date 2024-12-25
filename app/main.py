04de1c0d1f2257953072dade4ea3a257c69c1697
Tram Tran <ntt.tram.tran@hotmail.com>
2024-12-26 08:56:55 +1100

    Create upload file and router for scalability

--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile ## upload file
+from .routers import router
 
 
+## Setup FastAPI instance
 app = FastAPI()
+
+
+app.innclude_router(router)
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the FastAPI API!"}
+
