0e19dacb6679298eb697c598d464ec9014c009c1
Tram Tran <ntt.tram.tran@hotmail.com>
2024-12-29 17:24:18 +1100

    Add Tensorflow/OpenCV to router/main code

--- app/router.py
+++ app/router.py
@@ -1,7 +1,17 @@
 from fastapi import APIRouter, UploadFile, File
 import uuid
 from fastapi.responses import JSONResponse
+import tensorflow as tf
+import numpy as np
+import cv2
 router = APIRouter()
+
+
+# Load the pre-trained model
+model_dir = tf.saved_model.load('/app/data/modelset/ssd_mobilenet_v2_320x320_coco17_tpu-8/saved_model/')
+model = tf.saved_model.load(model_dir)
+
+
 
 @router.post("/upload/")
 async def create_upload_file(file: UploadFile = File(...)):
@@ -11,21 +21,38 @@ async def create_upload_file(file: UploadFile = File(...)):
 
 
 @router.post("/detect/")
-async def detect_image(file: UploadFile = File(...)):
-    # Check file type
-    if not file.filename.endswith(('.png', '.jpg', '.jpeg')):
-        return JSONResponse(status_code=400, content={"message" : "Invalid file format"})
+# async def detect_image(file: UploadFile = File(...)):
+#     # Check file type
+#     if not file.filename.endswith(('.png', '.jpg', '.jpeg')):
+#         return JSONResponse(status_code=400, content={"message" : "Invalid file format"})
     
-    # Save file to a secure location
-    file_location = f"/app/data/img/{str(uuid.uuid4())}-{file.filename}"
-    with open(file_location, "wb+") as file_object:
-        file_object.write(await file.read())
+#     # Save file to a secure location
+#     file_location = f"/app/data/img/{str(uuid.uuid4())}-{file.filename}"
+#     with open(file_location, "wb+") as file_object:
+#         file_object.write(await file.read())
         
-    # Perform image detection, recognition, and segmentation
-    results = process_image(file_location) # Placeholder for the actual processing function
+#     # Perform image detection, recognition, and segmentation
+#     results = process_image(file_location) # Placeholder for the actual processing function
     
-    return results
+#     return results
 
+async def detect_image(file: UploadFile = File(...)):
+    contents = await file.read()
+    nparr = np.fromstring(contents, np.uint8)
+    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    
+    # Prepare the image for the model
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    resized_img = cv2.resize(img, (320, 320))
+    input_tensor = tf.convert_to_tensor([resized_img], dtype=tf.float32)
+    
+    # Inference
+    infer = model.signatures['serving_default']
+    outputs = infer(input_tensor)
+    
+    # Process outputs
+    
+    return {"message" : "Image processed successfully"}
 
 def process_image(image_path):
     # Implement image processing logic here
