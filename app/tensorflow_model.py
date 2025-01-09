8274e3cb5986b3da10a809eda48f42448cb0c8bc
Tram Tran <ntt.tram.tran@hotmail.com>
2025-01-09 19:21:30 +1100

    Cleaning up
    Delete unwanted files and folders

--- app/tensorflow_model.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-# Load the pre-trained model
-model = tf.saved_model.load('/app/data/modelset/ssd_mobilenet_v2_320x320_coco17_tpu-8.tar')
-
-# Prepare the model for inference
-infer = model.signatures['serving_default']
-
